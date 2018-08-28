f7b1d8318e1326fd232d2ccdc67a39c75262e926
III\shuweilin <swlin0328@gmail.com>
2018-08-28 18:15:22 +0800

    KMeans 08/28 Update

---
+++ clustering.py
@@ -0,0 +1,39 @@
+from sklearn.cluster import KMeans
+from mysql_model import sql4cluster
+import pandas as pd
+from time import strftime
+
+def read_data_from_csv(file_name):
+    df = pd.read_csv('./data/' + file_name + '.csv', header=0)
+    return df
+
+def km_clustering(file_name='max_min_sum_w_dataSet', num_cluster=5, model_name='Kmeans', to_pkl=True, to_sql=False):
+    km = KMeans(n_clusters=num_cluster, random_state=0)
+    df = read_data_from_csv(file_name)
+    input_X = df.iloc[:, 3:99]
+    
+    y_km = km.fit_predict(input_X)
+    center = km.cluster_centers_
+    
+    save_model(km)
+    save_center2csv(center)
+    save_cluster2csv_with_label(df, y_km)
+    return km
+
+def save_model(model, model_name='KMeans', to_pkl=True, to_sql=False):
+    model_storage = sql4cluster(model_name)
+    if to_sql:
+        model_storage.save2sql(model)     
+    if to_pkl:
+        model_storage.save_model_to_pkl(model)
+
+def save_center2csv(center):
+    group_center = pd.DataFrame(center)
+    group_center = group_center.rename(index=str, columns= dict((idx, 'period_' + str(idx+1)) for idx in range(center.shape[1])))
+    group_center['Reporttime'] = strftime('%Y-%m-%d %H:%M')
+    group_center.to_csv(r'./result/group_center.csv', index=True, index_label = 'Group_ID')
+
+def save_cluster2csv_with_label(df, label):
+    df.insert(loc=3, column='Group_ID', value=label)
+    df['Reporttime'] = strftime('%Y-%m-%d %H:%M')
+    df.to_csv(r'./result/cluster_with_label.csv', index=False)
